72054f987405be139d892102aa01c7165752657b
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-11-17 21:25:38 +0100

    add test_package

--- test_package/test_package.c
+++ test_package/test_package.c
@@ -1,3 +1,8 @@
-int main() {
-    return 0;
+#include <mysql.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("MySQL client version: %s\n", mysql_get_client_info());
+  return 0;
 }
